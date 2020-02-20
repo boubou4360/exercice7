be9100ab8c82e153b4434d53a1015822479a9d2f
Jeremie Bouchard <jeremieb4360@gmail.com>
2020-02-20 15:00:00 -0500

    modif footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
+				<?php
+				/* translators: %s: WordPress. */
+				//print("veille");
+				?>
+			</a>
